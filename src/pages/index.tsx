cf76717022185f0c7fae38665fdfc1f6cf19b041
DamianCenco <damian.cancela@cencosud.com>
2023-05-12 09:07:00 -0300

    Installed sharp to remove console warning

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,42 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import dynamic from 'next/dynamic';
 
-const Header = dynamic(() => import('headerFooter/header'), { ssr: false, loading: () => <p>Loading header...</p> })
-const Footer = dynamic(() => import('headerFooter/footer'), { ssr: false, loading: () => <p>Loading footer...</p>})
-const Home = dynamic(() => import('home/home'), { ssr: false, loading: () => <p>Loading home...</p> })
-const CartAside = dynamic(() => import('cart/cartAside'), { ssr: false, loading: () => <p>Loading cart...</p> })
+const Header = dynamic(() => import('headerFooter/header'), {
+  ssr: false,
+  loading: () => (
+    <p style={{ height: '80px', width: '100vw' }}>Loading header...</p>
+  ),
+});
+const Footer = dynamic(() => import('headerFooter/footer'), {
+  ssr: false,
+  loading: () => (
+    <p style={{ height: '80px', width: '100vw' }}>Loading footer...</p>
+  ),
+});
+const Home = dynamic(() => import('home/home'), {
+  ssr: false,
+  loading: () => (
+    <p style={{ height: '1500px', width: '100vw' }}>Loading home...</p>
+  ),
+});
+const CartAside = dynamic(() => import('cart/cartAside'), {
+  ssr: false,
+  loading: () => <></>,
+});
 
 export default function HomeApp() {
   return (
     <>
       <Head>
         <title>Easy.cl - Renueva el amor por tu hogar</title>
-        <meta name="description" content="Easy CL" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.webp" />
+        <meta name='description' content='Easy CL' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.webp' />
       </Head>
       <main>
-        <ErrorBoundary FallbackComponent={() => <></>}> 
+        <ErrorBoundary FallbackComponent={() => <></>}>
           <Header />
           <Home />
           <Footer />
@@ -26,5 +44,5 @@ export default function HomeApp() {
         </ErrorBoundary>
       </main>
     </>
-  )
+  );
 }
