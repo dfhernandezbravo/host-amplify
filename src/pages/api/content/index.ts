bb1cd478dde4280ab66e27a6a30248bcd8270328
Pablo Clemente <martin.clemente@cencosud.com>
2023-06-20 17:56:09 -0300

    Api content added

---
+++ src/pages/api/content/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import type { NextApiRequest, NextApiResponse } from 'next';
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+  try {
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_CMS_URL}/home`,
+      {
+        headers:{
+            apikey: `${process.env.NEXT_PUBLIC_CMS_API_KEY}`
+        }
+      }
+    );
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
