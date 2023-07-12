4099676bec7fbb344af7f79d68ab9d2b1ac4be4a
Damián Cancela <damian.cancela@cencosud.com>
2023-07-12 01:18:15 +0000

    Feature/animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,22 @@ import useAnalytics, { EventData } from '@/analytics/hooks/useAnalytics';
 import { WINDOWS_EVENTS } from '@/events';
 import detectOs from '@/helpers/detectOS';
 import dynamic from 'next/dynamic';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import HomeSkeleton from '@/presentation/components/layouts/HomeSkeleton/HomeSkeleton';
 import LogoLoader from '@/presentation/modules/LogoLoader/LogoLoader';
 import HeaderSkeleton from '@/presentation/components/layouts/HeaderSkeleton/HeaderSkeleton';
 import FooterSkeleton from '@/presentation/components/layouts/FooterSkeleton/FooterSkeleton';
 
-const Header = dynamic(() => import('headerFooter/headers'), {
+const Header = dynamic(() => import('headerFooter/header'), {
   ssr: false,
   loading: () => <HeaderSkeleton />,
 });
-const Footer = dynamic(() => import('headerFooter/footers'), {
+const Footer = dynamic(() => import('headerFooter/footer'), {
   ssr: false,
   loading: () => <FooterSkeleton />,
 });
-const Home = dynamic(() => import('home/homes'), {
+const Home = dynamic(() => import('home/home'), {
   ssr: false,
   loading: () => <HomeSkeleton />,
 });
@@ -30,6 +30,15 @@ const CartAside = dynamic(() => import('cart/cartAside'), {
 
 export default function HomeApp(props: any) {
   const { sendEvent } = useAnalytics();
+  const [showLogo, setShowLogo] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowLogo(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -49,6 +58,10 @@ export default function HomeApp(props: any) {
   useEffect(() => {
     document.addEventListener(WINDOWS_EVENTS.Analytics, handleAnalyticsEvent);
   }, [handleAnalyticsEvent]);
+
+  if (showLogo) {
+    return <LogoLoader />; // Render the component for 1 second
+  }
 
   return (
     <>
---
+++ src/presentation/components/atoms/Animation/Animation.tsx
@@ -0,0 +1,60 @@
+import { CSSProperties, useEffect, useState } from 'react';
+import Lottie from 'lottie-react';
+
+// Animations
+import Animations from '../../../../assets/animations/list.json';
+
+// Definitions
+export type Props = {
+  name: keyof typeof Animations;
+  loop?: boolean;
+  onComplete?: () => void;
+  onDestroy?: () => void;
+  onLoopComplete?: () => void;
+  style?: CSSProperties;
+};
+
+// Defaults values
+const Defaults = {
+  onComplete: undefined,
+  onDestroy: undefined,
+  onLoopComplete: undefined,
+  style: undefined,
+};
+
+const Animation = (props: Props) => {
+  // Props
+  const { name, loop, style, onComplete, onDestroy, onLoopComplete } = props;
+
+  // State
+  const [animation, setAnimation] = useState();
+
+  // Methods
+  const methods = {
+    getAnimation: async () => {
+      const json = Animations[name];
+      const file = await import(`../../../../assets/animations/${json}`);
+      if (file) setAnimation(file);
+    },
+  };
+
+  // On name change
+  useEffect(() => {
+    methods.getAnimation();
+  }, [name]);
+
+  return (
+    <Lottie
+      animationData={animation}
+      loop={loop}
+      style={style}
+      onComplete={onComplete}
+      onLoopComplete={onLoopComplete}
+      onDestroy={onDestroy}
+    />
+  );
+};
+
+Animation.defaultProps = Defaults;
+
+export default Animation;
