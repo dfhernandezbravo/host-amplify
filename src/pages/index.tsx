b153c812496566b89b8835f14ad9a1c3b6c207d1
EH0284 <eliana.ormenosilva@externos-cl.cencosud.com>
2023-08-04 13:16:13 -0400

    fix: add stopImmediatePropagation to handleAnalyticsEvent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,30 @@
-import Head from 'next/head';
-import { ErrorBoundary } from 'react-error-boundary';
-import useAnalytics, { EventData } from '@/analytics/hooks/useAnalytics';
-import { WINDOWS_EVENTS } from '@/events';
-import detectOs from '@/helpers/detectOS';
-import dynamic from 'next/dynamic';
-import { useCallback, useEffect, useState } from 'react';
+import Head from "next/head";
+import { ErrorBoundary } from "react-error-boundary";
+import useAnalytics, { EventData } from "@/analytics/hooks/useAnalytics";
+import { WINDOWS_EVENTS } from "@/events";
+import detectOs from "@/helpers/detectOS";
+import dynamic from "next/dynamic";
+import { useCallback, useEffect, useState } from "react";
 
-import HomeSkeleton from '@/presentation/components/layouts/HomeSkeleton/HomeSkeleton';
-import LogoLoader from '@/presentation/modules/LogoLoader/LogoLoader';
-import HeaderSkeleton from '@/presentation/components/layouts/HeaderSkeleton/HeaderSkeleton';
-import FooterSkeleton from '@/presentation/components/layouts/FooterSkeleton/FooterSkeleton';
+import HomeSkeleton from "@/presentation/components/layouts/HomeSkeleton/HomeSkeleton";
+import LogoLoader from "@/presentation/modules/LogoLoader/LogoLoader";
+import HeaderSkeleton from "@/presentation/components/layouts/HeaderSkeleton/HeaderSkeleton";
+import FooterSkeleton from "@/presentation/components/layouts/FooterSkeleton/FooterSkeleton";
 
-const Header = dynamic(() => import('headerFooter/header'), {
+const Header = dynamic(() => import("headerFooter/header"), {
   ssr: false,
   loading: () => <HeaderSkeleton />,
 });
-const Footer = dynamic(() => import('headerFooter/footer'), {
+const Footer = dynamic(() => import("headerFooter/footer"), {
   ssr: false,
   loading: () => <FooterSkeleton />,
 });
-const Home = dynamic(() => import('home/home'), {
+const Home = dynamic(() => import("home/home"), {
   ssr: false,
   loading: () => <HomeSkeleton />,
 });
 
-const CartAside = dynamic(() => import('cart/cartAside'), {
+const CartAside = dynamic(() => import("cart/cartAside"), {
   ssr: false,
   loading: () => <></>,
 });
@@ -50,6 +50,7 @@ export default function HomeApp(props: any) {
   const handleAnalyticsEvent = useCallback(
     (event: Event) => {
       event.preventDefault();
+      event.stopImmediatePropagation();
       const customEvent = event as CustomEvent<EventData>;
       sendEvent(customEvent.detail);
     },
@@ -68,9 +69,9 @@ export default function HomeApp(props: any) {
     <>
       <Head>
         <title>Easy.cl - Renueva el amor por tu hogar</title>
-        <meta name='description' content='Easy CL' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.webp' />
+        <meta name="description" content="Easy CL" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.webp" />
       </Head>
       <main>
         <ErrorBoundary FallbackComponent={() => <></>}>
