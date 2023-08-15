265629f59818d3dfb062a8892e0788be1a2a1c85
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2023-08-15 16:44:02 -0300

    fix: fetch delivery error cors

--- src/pages/api/event/[event].ts
+++ src/pages/api/event/[event].ts
@@ -4,9 +4,13 @@ export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
+    //https://cl-ccom-cms-delivery.ecomm.cencosud.com/views/cl/easy/EasyWeb
   try {
+    console.log("key:",`${process.env.NEXT_PUBLIC_CMS_API_KEY}`)
+    const event: string = `${_req.query.event}`;
+    console.log("route:",`${ `${process.env.NEXT_PUBLIC_CMS_URL}/event/${event}`}`)
     const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_CMS_URL}/home-headless`,
+      `${process.env.NEXT_PUBLIC_CMS_URL}/home-headless/event/${event}`,
       {
         headers:{
             apikey: `${process.env.NEXT_PUBLIC_CMS_API_KEY}`
