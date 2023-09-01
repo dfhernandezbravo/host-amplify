a7ebfd6cc037ed2dcb126ff01a4960d0f61c0098
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2023-09-01 16:48:42 +0000

    feat: Config env files

--- src/pages/api/products/search/popular.ts
+++ src/pages/api/products/search/popular.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from 'axios';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
 export default async function handler(
   _req: NextApiRequest,
-  res: NextApiResponse<any>,
+  res: NextApiResponse<any>
 ) {
   try {
     const { data } = await axios.get(
-      `${process.env.NEXT_PUBLIC_BFF_MOBILE_URL_STG}/products/search/popular`,
+      `${process.env.NEXT_PUBLIC_BFF_MOBILE_URL}/products/search/popular`
     );
     res.json(data);
   } catch (error) {
