b3e37cd3c18e29d46be37c9d5026f4d4d4e27e01
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-10-17 10:48:24 -0300

    chore: adding promotions icons

--- src/pages/api/cart/getCartById/[cartId].ts
+++ src/pages/api/cart/getCartById/[cartId].ts
@@ -3,23 +3,22 @@ import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
-    _req: NextApiRequest,
-    res: NextApiResponse<any>,
+  _req: NextApiRequest,
+  res: NextApiResponse<any>,
 ) {
+  console.log(_req.query.cartId);
 
-    console.log(_req.query.cartId)
+  const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': process.env.NEXT_PUBLIC_API_KEY_BFF_WEB,
+  };
 
-    const headers= {
-        'Content-Type': 'application/json',
-        'x-api-key': process.env.NEXT_PUBLIC_API_KEY_BFF_WEB,
-      }
-    
-    try {
-        const request = `https://cl-ccom-easy-bff-web.ecomm-stg.cencosud.com/v1.1/shoppingcart/${_req.query.cartId}`
-        const response = await axios.get(request, {headers})
-        res.status(200).json(response.data)
-    } catch (error) {
-        console.log({error})
-    }
-    // res.status(200).json({ message: "healt test" });
+  try {
+    const request = `https://cl-ccom-easy-bff-web.ecomm-stg.cencosud.com/v1.1/shoppingcart/${_req.query.cartId}`;
+    const response = await axios.get(request, { headers });
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.log({ error });
+  }
+  // res.status(200).json({ message: "healt test" });
 }
