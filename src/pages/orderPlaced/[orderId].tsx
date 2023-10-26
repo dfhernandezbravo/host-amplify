5283ea1d114187cc9a27aa67100cfae9a2bba89d
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2023-10-26 18:40:21 -0300

    fix: next.config file

--- src/pages/orderPlaced/[orderId].tsx
+++ src/pages/orderPlaced/[orderId].tsx
@@ -2,7 +2,7 @@ import dynamic from 'next/dynamic';
 
 const OrderPlaced = dynamic(() => import('orderPlaced/orderPlaced'), {
   ssr: false,
-  loading: () => <></>,
+  loading: () => <h1>Cargando ...</h1>,
 });
 
 const OrderPlacedPage = () => <OrderPlaced />;
