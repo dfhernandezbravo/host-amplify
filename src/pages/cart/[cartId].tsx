9479219968333449f0cf1db2a647c09c700d8234
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2023-11-07 09:52:12 -0500

    feat: add props to header mfe

--- src/pages/cart/[cartId].tsx
+++ src/pages/cart/[cartId].tsx
@@ -1,5 +1,8 @@
+import { HeaderProps } from '@/@types/header-props';
 import HeaderSkeleton from '@/presentation/components/layouts/HeaderSkeleton/HeaderSkeleton';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { ParsedUrlQuery } from 'querystring';
 
 const RemoteCart = dynamic(() => import('cart/cart'), {
   ssr: false,
@@ -8,15 +11,35 @@ const RemoteCart = dynamic(() => import('cart/cart'), {
   ),
 });
 
-const Header = dynamic(() => import('headerFooter/header'), {
+const Header = dynamic<HeaderProps>(() => import('headerFooter/header'), {
   ssr: false,
   loading: () => <HeaderSkeleton />,
 });
 
+interface ParsedUrlQueryForPage extends ParsedUrlQuery {
+  cartId: string;
+}
+
 export default function Cart() {
+  const { query } = useRouter();
+  const { cartId } = query as ParsedUrlQueryForPage;
+  console.log(cartId);
+
   return (
     <>
-      <Header />
+      <Header
+        modules={{
+          logo: true,
+          location: true,
+          categories: false,
+          search: false,
+          login: false,
+          cart: false,
+          topBrands: false,
+          footerHeader: false,
+        }}
+        cartId={cartId}
+      />
       <RemoteCart />
     </>
   );
