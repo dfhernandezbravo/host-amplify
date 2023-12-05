aacf2086a0846ccf8698735c06f5d9f29affa9d6
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-12-05 08:41:09 -0300

    chore: modifying rating request

--- src/pages/api/vtex-ratings-and-reviews/[id].ts
+++ src/pages/api/vtex-ratings-and-reviews/[id].ts
@@ -2,6 +2,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
+type ReviewType = {
+  approved: boolean;
+  id: string;
+  locale: string;
+  location: null;
+  pastReviews: null;
+  productId: string;
+  rating: number;
+  reviewDateTime: string;
+  reviewerName: string;
+  searchDate: Date;
+  shopperId: string;
+  sku: null;
+  text: string;
+  title: string;
+  verifiedPurchaser: boolean;
+};
 export default async function getProductReviews(
   _req: NextApiRequest,
   res: NextApiResponse<Record<string, unknown>>,
@@ -16,10 +33,17 @@ export default async function getProductReviews(
     // eslint-disable-next-line no-debugger
     // console.log('response',response)
     // console.log('data', response.data.range)
+    const reviewCreated = response.data.data.map((review: ReviewType) => {
+      return {
+        ...review,
+        created: review.reviewDateTime,
+      };
+    });
+
     const newSignature = {
-      average: 2,
+      average: 4,
       range: response.data.range,
-      reviews: response.data.data,
+      reviews: reviewCreated,
     };
 
     res.status(200).json(newSignature);
