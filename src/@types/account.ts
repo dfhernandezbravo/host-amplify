ff1f7d08f2a4ea4fff04977b9b148a994141250e
Jose Martinez <josemanuel.martinez@cencosud.cl>
2023-12-15 10:13:07 -0300

    chore: translate to english the path

--- src/@types/account.ts
+++ src/@types/account.ts
@@ -1,15 +1,15 @@
 import { ComponentType } from 'react';
 
 export type AccountPaths =
-  | 'cotizacion-de-pedido'
-  | 'mis-favoritos'
-  | 'mis-tarjetas'
-  | 'pedidos'
-  | 'mi-organizacion'
-  | 'mi-perfil'
-  | 'autenticacion'
-  | 'direcciones'
-  | 'mis-compras';
+  | 'order-quotation'
+  | 'favorites'
+  | 'cards'
+  | 'orders'
+  | 'organization'
+  | 'profile'
+  | 'authentication'
+  | 'addresses'
+  | 'purchases';
 
 export type AccountComponentProps = {
   [key in AccountPaths]: ComponentType<{}>;
