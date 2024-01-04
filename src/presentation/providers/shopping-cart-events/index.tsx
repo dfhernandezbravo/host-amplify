bcba382732e331fbbb60c9181832ae47d5e2c86c
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-01-04 17:42:02 -0500

    feat: refresh cart id events

--- src/presentation/providers/shopping-cart-events/index.tsx
+++ src/presentation/providers/shopping-cart-events/index.tsx
@@ -1,12 +1,6 @@
-import { getShoppingCart } from '@/domain/use-cases/shopping-cart/get-cart';
-import { getCartId } from '@/domain/use-cases/shopping-cart/get-cart-id';
-import { useAppDispatch, useAppSelector } from '@/presentation/hooks/use-store';
+import useGetCartId from '@/domain/use-cases/shopping-cart/get-cart-id';
+import { useAppSelector } from '@/presentation/hooks/use-store';
 import React from 'react';
-import { useQuery } from 'react-query';
-import {
-  setCartId,
-  updateShoppingCart,
-} from '../store/modules/shopping-cart/slice';
 import WrapperEvents from './wrapper-events';
 
 interface Props {
@@ -15,25 +9,9 @@ interface Props {
 
 const ShoppingCartEvents: React.FC<Props> = ({ children }) => {
   const { cartId } = useAppSelector((state) => state.shoppingCart);
-  const dispatch = useAppDispatch();
+  const { fetchCartId } = useGetCartId();
 
-  const { data: orderFormId } = useQuery(['get-cart-id'], getCartId, {
-    enabled: !cartId,
-    onSuccess: (response) => {
-      dispatch(setCartId(response));
-    },
-  });
-
-  const { data: _data } = useQuery(
-    ['get-cart', orderFormId],
-    () => getShoppingCart(orderFormId),
-    {
-      enabled: !!orderFormId,
-      onSuccess: (response) => {
-        dispatch(updateShoppingCart(response));
-      },
-    },
-  );
+  if (!cartId) fetchCartId();
 
   return <WrapperEvents>{children}</WrapperEvents>;
 };
--- src/presentation/providers/shopping-cart-events/wrapper-events.tsx
+++ src/presentation/providers/shopping-cart-events/wrapper-events.tsx
@@ -1,6 +1,7 @@
 import { SHOPPING_CART_EVENTS } from '@/application/infra/events/shopping-cart';
 import useAddItemShoppingCart from '@/domain/use-cases/shopping-cart/add-item';
 import useDispatchCartId from '@/domain/use-cases/shopping-cart/dispatch-cart-id';
+import useGetCartId from '@/domain/use-cases/shopping-cart/get-cart-id';
 import useRemoveAllItemsShoppingCart from '@/domain/use-cases/shopping-cart/remove-all-items';
 import useRemoveItemShoppingCart from '@/domain/use-cases/shopping-cart/remove-item';
 import useUpdateShoppingCart from '@/domain/use-cases/shopping-cart/update-cart';
@@ -21,7 +22,8 @@ const WrapperEvents = ({ children }: Props) => {
   const { removeItemShoppingCart } = useRemoveItemShoppingCart();
   const { removeAllItemsShoppingCart } = useRemoveAllItemsShoppingCart();
   const { updateCart } = useUpdateShoppingCart();
-  const { dispatchGetCartId } = useDispatchCartId();
+  const { dispatchGetCartId, dispatchCartEvent } = useDispatchCartId();
+  const { refreshCartId } = useGetCartId();
 
   useEffect(() => {
     if (cartId && shoppingCart) {
@@ -48,6 +50,14 @@ const WrapperEvents = ({ children }: Props) => {
       document.addEventListener(
         SHOPPING_CART_EVENTS.DISPATCH_GET_CART_ID,
         dispatchGetCartId,
+      );
+      document.addEventListener(
+        SHOPPING_CART_EVENTS.DISPATCH_GET_CART,
+        dispatchCartEvent,
+      );
+      document.addEventListener(
+        SHOPPING_CART_EVENTS.REFRESH_CART_ID,
+        refreshCartId,
       );
     }
 
@@ -76,6 +86,14 @@ const WrapperEvents = ({ children }: Props) => {
         SHOPPING_CART_EVENTS.DISPATCH_GET_CART_ID,
         dispatchGetCartId,
       );
+      document.removeEventListener(
+        SHOPPING_CART_EVENTS.DISPATCH_GET_CART,
+        dispatchCartEvent,
+      );
+      document.removeEventListener(
+        SHOPPING_CART_EVENTS.REFRESH_CART_ID,
+        refreshCartId,
+      );
     };
   }, [
     cartId,
@@ -86,7 +104,17 @@ const WrapperEvents = ({ children }: Props) => {
     removeAllItemsShoppingCart,
     updateCart,
     dispatchGetCartId,
+    dispatchCartEvent,
+    refreshCartId,
   ]);
+
+  useEffect(() => {
+    dispatchCartEvent();
+  }, [dispatchCartEvent, shoppingCart]);
+
+  useEffect(() => {
+    dispatchGetCartId();
+  }, [dispatchGetCartId, cartId]);
 
   return children;
 };
