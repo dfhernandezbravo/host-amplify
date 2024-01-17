851ae23095cefca1e58ef987170c051d14317ac2
Carlos Pitre <carlospitre@Carloss-Mac-mini.local>
2024-01-17 13:58:35 -0500

    feat: event set password and validate access key

--- src/presentation/providers/auth/auth-events.tsx
+++ src/presentation/providers/auth/auth-events.tsx
@@ -1,4 +1,6 @@
 import { AUTH_EVENTS } from '@/application/infra/events/auth';
+import { useAccessKeyValidation } from '@/domain/use-cases/auth/accesskey-validation';
+import { useSetPassword } from '@/domain/use-cases/auth/set-password';
 import { useSignIn } from '@/domain/use-cases/auth/sign-in';
 import { useSignOut } from '@/domain/use-cases/auth/sign-out';
 import React, { useEffect } from 'react';
@@ -10,14 +12,29 @@ interface Props {
 const AuthEvents: React.FC<Props> = ({ children }) => {
   const { signOut } = useSignOut();
   const { signIn } = useSignIn();
+  const { setPassword } = useSetPassword();
+  const { accessKeyValidation } = useAccessKeyValidation();
 
   useEffect(() => {
     document.addEventListener(AUTH_EVENTS.DISPATCH_LOGOUT, signOut);
     document.addEventListener(AUTH_EVENTS.DISPATCH_SIGNIN, signIn);
+    document.addEventListener(AUTH_EVENTS.DISPATCH_SET_PASSWORD, setPassword);
+    document.addEventListener(
+      AUTH_EVENTS.DISPATCH_ACCESS_KEY_VALIDATION,
+      accessKeyValidation,
+    );
 
     return () => {
       document.removeEventListener(AUTH_EVENTS.DISPATCH_LOGOUT, signOut);
-      document.addEventListener(AUTH_EVENTS.DISPATCH_SIGNIN, signIn);
+      document.removeEventListener(AUTH_EVENTS.DISPATCH_SIGNIN, signIn);
+      document.removeEventListener(
+        AUTH_EVENTS.DISPATCH_SET_PASSWORD,
+        setPassword,
+      );
+      document.removeEventListener(
+        AUTH_EVENTS.DISPATCH_ACCESS_KEY_VALIDATION,
+        accessKeyValidation,
+      );
     };
   }, [signOut]);
 
