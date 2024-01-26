9cc0d6bc7df1662d27984f1d5a59ecaa3a925b5c
Luis <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2024-01-26 08:24:05 -0500

    feat: add analytics event on orderPlaced

--- src/pages/confirmation/index.tsx
+++ src/pages/confirmation/index.tsx
@@ -1,5 +1,8 @@
+import { WINDOWS_EVENTS } from '@/application/infra/events';
 import LogoLoader from '@/presentation/components/skeletons/LogoLoader/LogoLoader';
+import useAnalytics, { EventData } from '@/presentation/hooks/use-analytics';
 import dynamic from 'next/dynamic';
+import { useCallback, useEffect } from 'react';
 
 const Confirmation = dynamic(() => import('confirmation/confirmation'), {
   ssr: false,
@@ -7,6 +10,20 @@ const Confirmation = dynamic(() => import('confirmation/confirmation'), {
 });
 
 const ConfirmationPage = () => {
+  const { sendEvent } = useAnalytics();
+  const handleAnalyticsEvent = useCallback(
+    (event: Event) => {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      const customEvent = event as CustomEvent<EventData>;
+      sendEvent(customEvent.detail);
+    },
+    [sendEvent],
+  );
+
+  useEffect(() => {
+    document.addEventListener(WINDOWS_EVENTS.Analytics, handleAnalyticsEvent);
+  }, [handleAnalyticsEvent]);
   return <Confirmation />;
 };
 
