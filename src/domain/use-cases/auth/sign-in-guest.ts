83a1c72e223f21495061060e27f8775e26064919
Carlos Pitre <julio.pitremartinez@externos-cl.cencosud.com>
2024-02-13 09:32:42 -0500

    refactor: validate endpoint with accessToken

--- src/domain/use-cases/auth/sign-in-guest.ts
+++ src/domain/use-cases/auth/sign-in-guest.ts
@@ -3,8 +3,7 @@ import authService from '@/application/services/auth';
 
 export const signInGuest = async () => {
   try {
-    const response = await authService(bffPublicClient).authGuest();
-    const data = response?.data || { accessToken: null, refreshToken: null };
+    const { data } = await authService(bffPublicClient).authGuest();
     return data;
   } catch (error) {
     throw new Error(`${error}`);
