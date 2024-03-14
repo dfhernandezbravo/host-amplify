c1722bddb791b6ca6718c6833db0ded48033a633
Damian <damian.cancela@cencosud.com>
2024-03-14 15:13:53 -0300

    feat: add to cart with colors

--- src/domain/entities/product/index.ts
+++ src/domain/entities/product/index.ts
@@ -3,10 +3,11 @@ import { Product as ProductDS } from '@cencosud-ds/easy-design-system';
 export type PaintingCode = {
   code: string;
   hexColor: string;
+  quantity: number;
 };
 
 export type Product = ProductDS & {
   quantity: number;
   id: string;
-  paintingCode?: PaintingCode;
+  paintingCode: PaintingCode;
 };
