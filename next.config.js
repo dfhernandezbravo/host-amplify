c9dc0c2d242cb8065e060929520d25df1dc4635a
guillermoferrazcencosud <guillermo.ferrazcuello@cencosud.com>
2024-05-23 16:56:14 -0300

    feat: add seo tags component

--- next.config.js
+++ next.config.js
@@ -65,7 +65,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=31536000',
+            value: 'public, max-age=60',
           },
         ],
       },
